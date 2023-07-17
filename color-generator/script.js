5bc0249d51615a9d7f0ec357359fd364c0c5b831
Jen <jdp386@drexel.edu>
2023-07-17 15:33:46 -0400

    Add five boxes for color palette generation

--- color-generator/script.js
+++ color-generator/script.js
@@ -4,14 +4,30 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function() {
-    let hexCode = "#";
-    for (let i = 0; i < 6; i++) {
-        hexCode += hex[getRandomNumber()];
-    }
+window.onload = function () {
+    document.getElementById("btn").click();
+};
 
-    document.body.style.backgroundColor = hexCode;
+btn.addEventListener("click", function() {
+    let num = 1;
+    let current;
+    for (let i = 1; i < 6; i++) {
+        current = "color" + num;
+        let hexColor = generateColor();
+        document.getElementById(current).style.backgroundColor = hexColor;
+        document.getElementById(current).textContent = hexColor;
+        document.getElementById(current).style.fontSize = "x-large";
+        num++;
+    }
 })
+
+function generateColor() {
+    let hexColor = "#";
+    for (let j = 0; j < 6; j++) {
+        hexColor += hex[getRandomNumber()];
+    }
+    return hexColor;
+}
 
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
