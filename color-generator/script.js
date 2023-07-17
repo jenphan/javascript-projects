dd704791836b5dab18166c592ef704dae781f8b6
Jen <jdp386@drexel.edu>
2023-07-17 15:50:37 -0400

    Move js variables

--- color-generator/script.js
+++ color-generator/script.js
@@ -2,7 +2,6 @@ const colors = ["pink", "blue", "yellow"];
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('btn');
-const color = document.querySelector(".color");
 
 window.onload = function () {
     document.getElementById("btn").click();
@@ -10,19 +9,22 @@ window.onload = function () {
 
 btn.addEventListener("click", function() {
     let num = 1;
-    let current;
+
     for (let i = 1; i < 6; i++) {
-        current = "color" + num;
         let hexColor = generateColor();
+        let current = "color" + num;
+
         document.getElementById(current).style.backgroundColor = hexColor;
         document.getElementById(current).textContent = hexColor;
         document.getElementById(current).style.fontSize = "x-large";
+
         num++;
     }
 })
 
 function generateColor() {
     let hexColor = "#";
+
     for (let j = 0; j < 6; j++) {
         hexColor += hex[getRandomNumber()];
     }
